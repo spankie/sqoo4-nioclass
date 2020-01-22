7f7a3227635d595d3c1b87f17ddab4e97e48e470
Odohi David <odohi.david@gmail.com>
2020-01-22 22:43:40 +0100

    create a chat client without threads

---
+++ networking/src/app/single/Client.java
@@ -0,0 +1,42 @@
+package app.single;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * Client
+ */
+public class Client {
+  static boolean quit = false;
+  static final int port = 8081;
+  static final String hostname = "localhost";
+
+  public static void main(String[] args) {
+    System.out.println("client has started");
+    try (Socket socket = new Socket(hostname, port);
+        InputStream input = socket.getInputStream();
+        OutputStream output = socket.getOutputStream()) {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+      Console console = System.console();
+      while (!quit) {
+        String message = console.readLine();
+        if (message.equals("quit")) {
+          quit = true;
+        }
+        System.out.printf("Me: %s\n", message);
+        output.write((message + "\n").getBytes());
+        String serverMessage = reader.readLine();
+        System.out.printf("Server: %s\n", serverMessage);
+      }
+    } catch (IOException ex) {
+      System.out.println("Error getting input stream: " + ex.getMessage());
+      ex.printStackTrace();
+      // handle suppressed exceptions
+    }
+  }
+}
