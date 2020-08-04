706fece86e1f1710c37142ec227334f29b6694ba
Odohi David <odohi.david@gmail.com>
2020-08-04 12:20:33 +0100

    update app.java

--- sq04-javaio/src/app/App.java
+++ sq04-javaio/src/app/App.java
@@ -18,26 +18,32 @@ import java.util.Collections;
 public class App {
     public static void ReadWithInputStream() {
         String line;
-        // FileInputStream fin = null;
-        FileReader fin = null;
+        FileInputStream fin = null;
+        // FileReader fin = null;
         BufferedReader br = null;
         InputStreamReader isr = null;
         int b;
         try {
-            fin = new FileReader("./.classpath");
-            // fin = new FileInputStream("./.classpath");
-            while ((b = fin.read()) >= 0) {
-                // byte bb = 18909;
-                // System.out.println((byte) b); // check the character equivalent of each
-                System.out.println((byte) b);
-                // System.out.print((char) b);
-                // System.out.print(b);
-            }
-            // isr = new InputStreamReader(fin);
-            br = new BufferedReader(fin);
+            fin = new FileInputStream("sq04-javaio/.classpath");
+            // fin = new FileReader("./.classpath");
+            // while ((b = fin.read()) >= 0) {
+            // a byte = 8 bits
+            // byte bb = 18909;
+            // System.out.println((byte) b); // check the character equivalent of each
+            // System.out.printf("%d ", (byte) b);
+            // System.out.printf("%s\n", (char) b);
+            // System.out.print(b);
+            // }
+            // System.out.println("----");
+            isr = new InputStreamReader(fin);
+            // while ((line = isr.read()) >= 0) {
+            // System.out.println(line);
+            // }
+            br = new BufferedReader(isr);
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
+            fin.close();
         } catch (Exception e) {
             System.err.println("Exception: " + e.getMessage());
         } finally {
@@ -51,8 +57,29 @@ public class App {
 
     }
 
+    public static void WriteWithOutputStream() {
+        try {
+            OutputStream fout = new FileOutputStream("./file.txt");
+            // min -128 max 127
+            byte D = 68; // D
+            byte[] dad = { D, 65, D };
+            byte enter = 13; // carriage return or enter key
+            fout.write(D);
+            fout.write(D);
+            fout.write(enter);
+            fout.write(dad);
+            fout.write(enter);
+            fout.write(65); // A
+            fout.write(65); // A
+            fout.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.err.println("Exception: " + e.getMessage());
+        }
+    }
+
     public static void ReadWithNIO() {
-        Path path = Paths.get("./.classpath");
+        Path path = Paths.get("sq04-javaio/.classpath");
         try {
             List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
             for (String line : lines) {
@@ -75,7 +102,7 @@ public class App {
             for (int i = 65; i < 91; i++) {
                 try {
                     pipedOutputStream.write(i);
-                    System.out.print((char) pipedInputStream.read());
+                    System.out.printf("%s ", (char) pipedInputStream.read());
                 } catch (Exception e) {
                     // TODO: handle exception
                     System.err.println("Exception: " + e.getMessage());
@@ -85,60 +112,40 @@ public class App {
 
         } catch (Exception e) {
             System.err.println("Exception : " + e.getMessage());
+        } finally {
+            System.out.println("finally block was called");
         }
-        // finally {
-        // try {
-        // pipedInputStream.close();
-        // } catch (Exception e) {
-        // System.err.println("Exception: " + e.getMessage());
-        // }
-        // }
-    }
-
-    public static void WriteWithOutputStream() {
-        try {
-            OutputStream fout = new FileOutputStream("./file.txt");
-            // OutputStream fout = new (20);
-            byte b = 68;
-            byte enter = 13;
-            fout.write(b);
-            fout.write(b);
-            fout.write(enter);
-            fout.write(enter);
-            fout.write(65);
-            fout.write(65);
-            fout.close();
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.err.println("Exception: " + e.getMessage());
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        // ReadWithInputStream();
-        // ReadWithNIO();
-        // WriteWithOutputStream();
-        PipedStream();
-        // DoTryWithResource();
     }
 
     public static void DoTryWithResource() {
         // MyAutoClosable ma = new MyAutoClosable();
 
+        // try {
         try (MyAutoClosable ma = new MyAutoClosable()) {
             ma.DoSomething();
         } catch (Exception e) {
             System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+
             for (Throwable t : e.getSuppressed()) {
                 System.out.println("getSuppressed() - " + t.getClass().getSimpleName() + " - " + t.getMessage());
             }
         }
         // finally {
-        // try {
-        // ma.close();
-        // } catch (Exception e) {
-        // System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        //     try {
+        //         ma.close();
+        //     } catch (Exception e) {
+        //         System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        //     }
         // }
-        // }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        // ReadWithInputStream();
+        // WriteWithOutputStream();
+        // ReadWithNIO();
+        // PipedStream();
+        // DoTryWithResource();
+        // env = prod
     }
 }
