acbfce2833f56a65809ff6c3974ec0ca4a688f49
Odohi David <odohi.david@gmail.com>
2020-08-25 18:57:54 +0100

    some text changes to the inetaddress example

--- networking/src/app/inet/InetAddressExample.java
+++ networking/src/app/inet/InetAddressExample.java
@@ -10,12 +10,17 @@ import java.net.*;
 public class InetAddressExample {
   public static void main(String[] args) {
     try {
+      // 127.0.0.1:3306
+      // 0.0.0.0:3306
       InetAddress address = InetAddress.getLocalHost();
-      System.out.println(address.getHostAddress());
-      System.out.println(address.getHostName());
+      System.out.printf("host address of localhost: %s\n", address.getHostAddress());
+      System.out.printf("host name of localhost: %s\n\n\n", address.getHostName());
+
+
       InetAddress address2 = InetAddress.getByName("google.com");
-      System.out.println(address2.getHostAddress());
-      System.out.println(address2.getHostName());
+      System.out.printf("host address of google.com: %s\n", address2.getHostAddress());
+      System.out.printf("host name of google.com: %s\n", address2.getHostName());
+      
     } catch (IOException e) {
       System.out.println(e.toString());
     }
