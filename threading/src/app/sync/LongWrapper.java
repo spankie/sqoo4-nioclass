946d1593347c2db41539355fa344f45fb5288d7c
Odohi David <odohi.david@gmail.com>
2020-08-25 19:01:13 +0100

    update comments

--- threading/src/app/sync/LongWrapper.java
+++ threading/src/app/sync/LongWrapper.java
@@ -16,12 +16,12 @@ public class LongWrapper {
   }
 
   public void incrementValue() {
-    // synchronized (key) {
+    synchronized (key) {
       // System.out.println(l);
       l = l + 1;
-      // 1. get the value of l 
+      // 1. get the initial value of l 
       // 2. add 1 to the value of l 
       // 3. set the value of l to the sum we get from step 2
-    // }
+    }
   }
 }
