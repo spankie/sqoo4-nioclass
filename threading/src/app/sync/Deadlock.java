5430fafd62f3b0b69f9b9771ce078caea6747f31
Odohi David <odohi.david@gmail.com>
2020-08-25 19:00:49 +0100

    some refactor and renaming of variables

--- threading/src/app/sync/Deadlock.java
+++ threading/src/app/sync/Deadlock.java
@@ -1,41 +1,49 @@
 package app.sync;
 
 public class Deadlock {
-  public static void main(String[] args) {  
-    final String resource1 = "ratan jaiswal";  
-    final String resource2 = "vimal jaiswal";  
-    // t1 tries to lock resource1 then resource2  
-    Thread t1 = new Thread() {  
-      public void run() {  
-          synchronized (resource1) {  
-           System.out.println("Thread 1: locked resource 1");  
-  
-           try { Thread.sleep(100); } catch (Exception e) {}  
-  
-           synchronized (resource2) {  
-            System.out.println("Thread 1: locked resource 2");  
-           }  
-         }  
-      }  
-    };  
-  
-    // t2 tries to lock resource2 then resource1  
-    Thread t2 = new Thread() {  
-      public void run() {  
-        synchronized (resource2) {  
-          System.out.println("Thread 2: locked resource 2");  
-  
-          try { Thread.sleep(100); } catch (Exception e) {}  
-  
-          synchronized (resource1) {  
-            System.out.println("Thread 2: locked resource 1");  
-          }  
-        }  
-      }  
-    };  
-  
-      
-    t1.start();  
-    t2.start();  
-  }  
-} 
+  private String key = "res";
+  public static void main(String[] args) {
+    final String key1 = "spankie";
+    final String key2 = "dee";
+    // t1 tries to lock key1 then key2
+    Thread t1 = new Thread() {
+      public void run() {
+        synchronized (key1) {
+          System.out.println("Thread 1: locked resource 1");
+
+          try {
+            Thread.sleep(100); // mimick some workload
+          } catch (Exception e) {
+          }
+
+          /// waiting for key2
+          synchronized (key2) {
+            System.out.println("Thread 1: locked resource 2");
+          }
+        }
+      }
+    };
+
+    // t2 tries to lock key2 then key1
+    Thread t2 = new Thread() {
+      public void run() {
+        synchronized (key2) {
+          System.out.println("Thread 2: locked resource 2");
+
+          try {
+            Thread.sleep(100); 
+          } catch (Exception e) {
+          }
+
+          // waiting...
+          synchronized (key1) {
+            System.out.println("Thread 2: locked resource 1");
+          }
+        }
+      }
+    };
+
+    t1.start();
+    t2.start();
+  }
+}
